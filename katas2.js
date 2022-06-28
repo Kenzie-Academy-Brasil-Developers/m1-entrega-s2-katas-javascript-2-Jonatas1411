366a0e7ca83dd867c6fe27520932388c656bdef8
Jonatas Andrade <106789706+Jonatas1411@users.noreply.github.com>
2022-06-28 13:05:31 -0300

    Add files via upload

--- katas2.js
+++ katas2.js
@@ -1,29 +1,49 @@
 // comece a criar a sua função add na linha abaixo
+function add(num1, num2){
+    return num1 + num2;
 
-
-// descomente a linha seguinte para testar sua função
-// console.assert(add(3, 5) === 8, 'A função add não está funcionando como esperado');
+    
+}
+console.log(add(3,5))
+ console.assert(add(3, 5) === 8,);
 
 
 // comece a criar a sua função multiply na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(multiply(4, 6) === 24, 'A função multiply não está funcionando como esperado');
+function multiply(num1, num2){
+    let mult = 0
+    for (let i = 0; i <num2; i++){
+        mult += add(0, num1)
+    }
+   return mult
+}
+console.log(multiply (4,6))
+ console.assert(multiply(4, 6) === 24,);
 
 
 // comece a criar a sua função power na linha abaixo
+function power (num1, num2 ){
+    let poweer = 1;
 
-
-// descomente a linha seguinte para testar sua função
-// console.assert(power(3, 4) === 81, 'A função power não está funcionando como esperado');
+    for(let i = 0; i < num2; i++){
+        poweer = multiply(num1,poweer)
+    }
+    return poweer
+}
+console.log(power(3,4))
+console.assert(power(3, 4) === 81,);
 
 
 // comece a criar a sua função factorial na linha abaixo
-
-
-// descomente a linha seguinte para testar sua função
-// console.assert(factorial(5) === 120, 'A função factorial não está funcionando como esperado');
+function factorial(fac1){
+    let factor = 1;
+    
+    for(let i = 1; i <= fac1; i++){
+        factor = multiply(factor, i)
+    }
+    return factor
+}
+console.log(factorial(5))
+console.assert(factorial(5) === 120,);
 
 
 /**
